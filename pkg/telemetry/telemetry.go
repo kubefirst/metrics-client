3fc0ba7c1b82783d5e544cef97bd86d07d972eaa
Jared Edwards <jared@kubefirst.com>
2023-10-23 12:02:23 -0600

    add v2 function to test heartbeats

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -43,3 +43,41 @@ func SendEvent(segmentClient *SegmentClient, metricName string, errMsg string) e
 
 	return nil
 }
+
+func SendEventV2(segmentIOWriteKey string, event TelemetryEvent, metricName string, errMsg string) error {
+
+	client := analytics.New(segmentIOWriteKey)
+	defer client.Close()
+
+	if event.MetricName == ClusterInstallStarted {
+		err := client.Enqueue(analytics.Identify{
+			UserId: event.UserId,
+			Type:   "identify",
+		})
+		if err != nil {
+			return err
+		}
+	}
+	err := client.Enqueue(analytics.Track{
+		UserId: event.UserId,
+		Event:  metricName,
+		Properties: analytics.NewProperties().
+			Set("cli_version", event.CliVersion).
+			Set("cloud_provider", event.CloudProvider).
+			Set("cluster_id", event.ClusterID).
+			Set("cluster_type", event.ClusterType).
+			Set("domain", event.DomainName).
+			Set("git_provider", event.GitProvider).
+			Set("client", event.KubefirstClient).
+			Set("kubefirst_team", event.KubefirstTeam).
+			Set("kubefirst_team_info", event.KubefirstTeamInfo).
+			Set("machine_id", event.MachineID).
+			Set("error", errMsg).
+			Set("install_method", event.InstallMethod),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
